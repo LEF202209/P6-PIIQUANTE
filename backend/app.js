aa7c053bf363b9a2ff0ffab859ea0072de320dab
Ange PA <ange.lef202209@gmail.com>
2023-10-25 16:04:53 +0200

    Setting up user signup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 //importation du module de variable d'environnement
 require('dotenv').config();
+// importer body-parser
+const bodyParser = require('body-parser');
 // importer express module
 const express = require('express');
 // créer une application express
 const app = express();
 // importer mongoose
 const mongoose = require('mongoose');
+// importer des fichiers de routing user
+const userRoutes = require('./routes/user')
 
 // variables d'environnement
 const userName= process.env.DB_USERNAME
@@ -26,5 +30,7 @@ mongoose.connect(`mongodb+srv://${userName}:${password}@${accessMongo}/?retryWri
 .then(()=> console.log('Connexion à MongoDB réussie !'))
 .catch(()=> console.log('Connexion à MongoDB échouée!'));
 
+app.use(bodyParser.json());
+app.use('/api/auth',userRoutes);
 // exporter l'applicat° pour qu'on puisse y accéder depuis les autres fichiers notemment notre server node //
 module.exports = app;
---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+/***********************************************************************************************/
+/**************************************       Modèle User                ***********************/
+/***********************************************************************************************/
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+// Définition schéma mongoose user //
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true}
+})
+
+// Appliquer le plugin uniqueValidator au schéma //
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', userSchema)
